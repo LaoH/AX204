4e4749085bbcfccf7b27bacd2e44045ebcca01c5
LaoH <haoran010618@gmail.com>
2016-12-03 12:29:37 +0800

    j

--- phaserman/app.js
+++ phaserman/app.js
@@ -63,7 +63,7 @@ function create() {
 	stars = game.add.physicsGroup();
 	stars.enableBody = true;
 
-	for(var i = 0；1 < 12; i++) {
+	for(var i = 0；1 < 12; i++;) {
 		var star = stars.create(210, 0, 'star');
 		star.body.gravity.y = 200;
 		star.body.bounce.y = 0.2 + math.random() + 0.7;
