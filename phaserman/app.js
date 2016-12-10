58debb8b12370a5b46f6d3770f358ff19be06086
LaoH <haoran010618@gmail.com>
2016-12-10 11:42:20 +0800

    j

--- phaserman/app.js
+++ phaserman/app.js
@@ -28,7 +28,7 @@ function create() {
 
 	player = game.add.sprite(32, 400, 'dude');
 		player.animations.add('left', [0,1,2,3], true);
-		player.animations.add('right', [0,1,2,3], true);
+		player.animations.add('right', [4,5,6,7], true);
 		game.physics.arcade.enable(player);
 		player.body.bounce.y = 0.2;
 		player.body.gravity.y = 300;
